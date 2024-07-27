22db9d09e1fe612a365bdf3cd4b0e9f270e7e927
Rand Ruggles <54882841+RRuggles9@users.noreply.github.com>
2024-07-26 23:24:06 -0400

    Sprint1 Commit 1

--- CICDDemo/Form1.Designer.cs
+++ CICDDemo/Form1.Designer.cs
@@ -28,20 +28,44 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbProjName = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // tbProjName
+            // 
+            this.tbProjName.Location = new System.Drawing.Point(74, 17);
+            this.tbProjName.Name = "tbProjName";
+            this.tbProjName.Size = new System.Drawing.Size(100, 20);
+            this.tbProjName.TabIndex = 1;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tbProjName);
+            this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "CICDDemo";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbProjName;
     }
 }
 
